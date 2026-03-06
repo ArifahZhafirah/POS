beff83c2e15ee020476c95a88e5ad59af7437e15
ArifahZhafirah <arifahzhafirah206@gamil.com>
2026-03-06 10:07:46 +0700

    Praktikum 4 DB Facade

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,33 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SalesController;
+// use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\ProductController;
+// use App\Http\Controllers\SalesController;
+
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-//Home
-Route:: get('/', [HomeController::class, 'index']);
-
-//product
-Route:: prefix ('category')->group(function(){
-    Route:: get ('/food-beverage', [ProductController::class, 'food']);
-    Route:: get ('/beauty-health', [ProductController::class, 'beauty']);
-    Route:: get ('/home-care', [ProductController::class, 'home']);
-    Route:: get ('/baby-kid', [ProductController::class, 'baby']);
+Route::get('/', function(){
+    return view('welcome');
 });
 
-//user
-Route::get ('/user/{id}/name/{name}', [UserController::class, 'profile']);
-
-//Penjualan
-Route::get ('/sales', [SalesController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
 
 
+//Home
+// Route:: get('/', [HomeController::class, 'index']);
+
+// //product
+// Route:: prefix ('category')->group(function(){
+//     Route:: get ('/food-beverage', [ProductController::class, 'food']);
+//     Route:: get ('/beauty-health', [ProductController::class, 'beauty']);
+//     Route:: get ('/home-care', [ProductController::class, 'home']);
+//     Route:: get ('/baby-kid', [ProductController::class, 'baby']);
+// });
+
+// //user
+// Route::get ('/user/{id}/name/{name}', [UserController::class, 'profile']);
+
+// //Penjualan
+// Route::get ('/sales', [SalesController::class, 'index']);
