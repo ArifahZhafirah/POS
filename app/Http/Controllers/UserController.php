e46b9758c83ec490faf370034d841f3fc4f3caed
ArifahZhafirah <arifahzhafirah206@gamil.com>
2026-03-10 20:06:43 +0700

    Praktikum 2.3 Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,10 +14,11 @@ class UserController extends Controller
     public function index()
     {
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
         return view ('user', ['data' => $user]);
     }
 }
+
 
 
 //  $data = [
--- resources/views/User.blade.php
+++ resources/views/User.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html>
     <head>
         <title>Data User</title>
@@ -17,6 +17,21 @@
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->nama }}</td>
                 <td>{{ $data->level_id }}</td>
+            </tr>
+        </table>
+    </body>
+</html> --}}
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Data User</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>Jumlah Pengguna</th>
+            </tr>
+            <tr>
+                <td>{{ $data }}</td>
             </tr>
         </table>
     </body>
